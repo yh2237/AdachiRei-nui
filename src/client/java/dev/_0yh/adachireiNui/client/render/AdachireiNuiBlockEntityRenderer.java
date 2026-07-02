f0bd9575597024ea9a97635a69198d4716774a97
yh2237 <2237.yh.2237@gmail.com>
2026-07-02 18:56:24 +0900

    overlayを追加

--- src/client/java/dev/_0yh/adachireiNui/client/render/AdachireiNuiBlockEntityRenderer.java
+++ src/client/java/dev/_0yh/adachireiNui/client/render/AdachireiNuiBlockEntityRenderer.java
@@ -10,6 +10,7 @@ import dev._0yh.adachireiNui.block.entity.AdachireiNuiBlockEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.VertexConsumer;
@@ -101,10 +102,10 @@ public class AdachireiNuiBlockEntityRenderer implements BlockEntityRenderer<Adac
 
                 queue.submitCustom(matrices, renderLayer, (entry, consumer) -> {
                     Matrix4f position = entry.getPositionMatrix();
-                    vertex(consumer, position, entry, vertices[0], uv0[0], uv0[1], light, normal);
-                    vertex(consumer, position, entry, vertices[1], uv1[0], uv1[1], light, normal);
-                    vertex(consumer, position, entry, vertices[2], uv2[0], uv2[1], light, normal);
-                    vertex(consumer, position, entry, vertices[3], uv3[0], uv3[1], light, normal);
+                    vertex(consumer, position, entry, vertices[0], uv0[0], uv0[1], light, OverlayTexture.DEFAULT_UV, normal);
+                    vertex(consumer, position, entry, vertices[1], uv1[0], uv1[1], light, OverlayTexture.DEFAULT_UV, normal);
+                    vertex(consumer, position, entry, vertices[2], uv2[0], uv2[1], light, OverlayTexture.DEFAULT_UV, normal);
+                    vertex(consumer, position, entry, vertices[3], uv3[0], uv3[1], light, OverlayTexture.DEFAULT_UV, normal);
                 });
             }
         }
@@ -298,10 +299,11 @@ public class AdachireiNuiBlockEntityRenderer implements BlockEntityRenderer<Adac
         return corners[sourceIndex];
     }
 
-    private static void vertex(VertexConsumer consumer, Matrix4f position, MatrixStack.Entry entry, Vector3f vertex, float u, float v, int light, Vector3f normal) {
+    private static void vertex(VertexConsumer consumer, Matrix4f position, MatrixStack.Entry entry, Vector3f vertex, float u, float v, int light, int overlay, Vector3f normal) {
         consumer.vertex(position, vertex.x, vertex.y, vertex.z)
                 .color(255, 255, 255, 255)
                 .texture(u, v)
+                .overlay(overlay)
                 .light(light)
                 .normal(entry, normal.x, normal.y, normal.z);
     }
