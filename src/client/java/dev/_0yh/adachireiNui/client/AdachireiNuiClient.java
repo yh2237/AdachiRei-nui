78453c7e0fea55ab2dcf0a91e5eba405f747a31f
yh2237 <2237.yh.2237@gmail.com>
2026-07-02 23:24:26 +0900

    1.21.5に対応

--- src/client/java/dev/_0yh/adachireiNui/client/AdachireiNuiClient.java
+++ src/client/java/dev/_0yh/adachireiNui/client/AdachireiNuiClient.java
@@ -5,8 +5,6 @@ import dev._0yh.adachireiNui.client.render.AdachireiNuiBlockEntityRenderer;
 import dev._0yh.adachireiNui.config.AdachireiConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.BlockRenderLayer;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
@@ -15,12 +13,6 @@ public class AdachireiNuiClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         AutoConfig.register(AdachireiConfig.class, GsonConfigSerializer::new);
-
-        BlockRenderLayerMap.putBlocks(
-                BlockRenderLayer.CUTOUT,
-                AdachireiNui.ADACHI_BLOCK_5,
-                AdachireiNui.ADACHI_BLOCK_6,
-                AdachireiNui.ADACHI_BLOCK_7);
 
         BlockEntityRendererRegistry.register(
                 AdachireiNui.ADACHIREI_NUI_BLOCK_ENTITY_TYPE,
