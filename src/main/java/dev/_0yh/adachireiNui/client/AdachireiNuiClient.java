ff5855d084e0e6c8a3435a2dbb69de9c7e5825b0
yh2237 <2237.yh.2237@gmail.com>
2026-08-02 20:40:49 +0900

    NeoForge 1.21.3版を追加

--- src/main/java/dev/_0yh/adachireiNui/client/AdachireiNuiClient.java
+++ src/main/java/dev/_0yh/adachireiNui/client/AdachireiNuiClient.java
@@ -3,16 +3,29 @@ package dev._0yh.adachireiNui.client;
 import dev._0yh.adachireiNui.AdachireiNui;
 import dev._0yh.adachireiNui.client.render.AdachireiNuiBlockEntityRenderer;
 import dev._0yh.adachireiNui.config.AdachireiConfig;
+import dev._0yh.adachireiNui.item.AdachireiNuiBlockItem;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
 @OnlyIn(Dist.CLIENT)
 public class AdachireiNuiClient {
+
+    public static void onRegisterClientExtensions(final RegisterClientExtensionsEvent event) {
+        for (String name : new String[] {"adachi-nui", "adachi-nui_new", "adachi-nui_vocaloid"}) {
+            var item = BuiltInRegistries.ITEM.getValue(ResourceLocation.fromNamespaceAndPath(AdachireiNui.CONTENT_NAMESPACE, name));
+            if (item instanceof AdachireiNuiBlockItem customItem) {
+                event.registerItem(customItem.createClientExtensions(), customItem);
+            }
+        }
+    }
 
     public static void onClientSetup(final FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
